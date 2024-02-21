c3533bd9d2fab470e364a8d5fac8439c84e3d228
GannaTarasenko <maracheva.ganna@gmail.com>
2024-02-21 19:26:30 +0100

    com_3

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 print("Hello World")
 
 print("Hello Git")
+
+message = "Hello world!"
+
+name = "Hanna"
+hello_string = f"Hello, {name}!"
+print(hello_string)
+
+n = 5000
+
+hours = n // (60 * 60)
+minutes = (n - hours * 60 * 60) // 60
+seconds = n - hours * 60 * 60 - minutes * 60
+
+a = input("Сьогодні буде гарний день: ")
+# На екрані ви побачите: Сьогодні буде гарний день:
+
+
+
+
+
+
